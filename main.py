65cb95dde9048a64ab71c1a180fd2f5623788ab8
kertys35 <kertys50@gmail.com>
2026-05-25 22:55:18 +0700

    Создан api для классификации обзоров yelp

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from translate import Translator
+from transformers import pipeline
+from pydantic import BaseModel
+
+class UserRequest(BaseModel):
+    text: str
+
+app = FastAPI()
+
+translator= Translator(from_lang="russian", to_lang="english")
+
+
+async def classify_text(text):
+    classifier = pipeline("sentiment-analysis", model="kertys/yelp_review_classifier")
+    translator= Translator(from_lang="russian", to_lang="english")
+    translation = translator.translate(text)
+    return classifier(translation)
+
+@app.get("/")
+async def root():
+    return {"message": "API is running"}
+
+@app.post("/classification")
+async def classify(prompt: UserRequest):
+    result = await classify_text(prompt.text)
+    return JSONResponse({"Результат анализа:": result})
